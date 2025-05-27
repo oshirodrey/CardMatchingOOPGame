132b646730f2c5af49a935a2dae15215d829a7b8
OshiroDrey <minhtoan2746@gmail.com>
2025-05-27 17:09:56 +0700

    add game presenter
    It should be controlled by game controller to tell what the game screen should do

--- src/CardGame/InterfaceAdapters/Presenter/GamePresenter.java
+++ src/CardGame/InterfaceAdapters/Presenter/GamePresenter.java
@@ -1,4 +1,27 @@
 package CardGame.InterfaceAdapters.Presenter;
 
+import CardGame.Domain.Entities.GameBoard;
+import CardGame.Domain.Entities.Score;
+import CardGame.InterfaceAdapters.IUIController;
+
 public class GamePresenter {
+    private final GameBoard gameBoard;
+    private final IUIController ui;
+
+    public GamePresenter(GameBoard gameBoard, IUIController ui) {
+        this.gameBoard = gameBoard;
+        this.ui = ui;
+    }
+
+    public void presentFlip() {
+        ui.updateCardIcons();
+    }
+
+    public void presentStatus() {
+        ui.updateMoveAndTime(gameBoard.getMoveCount(), gameBoard.getElapsedTime());
+
+        if (gameBoard.isAllCardsMatched()) {
+            ui.showWinScreen(gameBoard.getScoreSnapshot());
+        }
+    }
 }
