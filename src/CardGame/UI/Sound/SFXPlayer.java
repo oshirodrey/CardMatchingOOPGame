c2427758926bb0d71190741413bd8f6c7e3ec07e
OshiroDrey <minhtoan2746@gmail.com>
2025-05-31 08:44:22 +0700

    create classes for sound

---
+++ src/CardGame/UI/Sound/SFXPlayer.java
@@ -0,0 +1,20 @@
+package CardGame.UI.Sound;
+
+import CardGame.InterfaceAdapters.ISoundPlayer;
+
+public class SFXPlayer implements ISoundPlayer {
+    @Override
+    public void play(String soundID) {
+
+    }
+
+    @Override
+    public void stop() {
+
+    }
+
+    @Override
+    public void setMuted(boolean muted) {
+
+    }
+}
